33a93bba8e58af93bea69dc43786274278dc0154
darkopjevic <61805490+darkopjevic@users.noreply.github.com>
2020-05-03 19:03:09 +0200

    Add files via upload

---
+++ thread_count arg/Merge_VCF_merge_9.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+"""
+SmartCombineVariants (SCV) v1.0 made with Python by Zorana Štaka and Darko Pjević
+This tools performs combining of two or more vcf files into one according to the perspective rules.
+Copyright (c) ETF Beograd
+
+Usage: Merge_VCF_merge_9.py (-i <inputVCF.vcf>)... [-s <sample_name>]... [-t <thread_count>] [-f <output_format>] [-o <out>] 
+
+Options:
+  -h --help
+  
+  -i,--input_file <inputVCF.vcf>                                        Input vcf file
+
+  -s, --sample_name <sample_name>                                       Name of the sample(s) to be combined
+  
+  -t, --thread_count <thread_count>                                     Number of threads
+
+  -f,--output_format <output_format>                                    Output file format:
+                                                                        COMPRESSED, UNCOMPRESSED or SAME_AS_INPUT
+  
+  -o,--out <out>                                                        Output file
+
+"""
+
+import time
+from docopt import docopt
+from Output_file import Output_file
+import profile
+
+start_time = time.time()
+output_file = None
+
+"""
+    Collecting arguments from input line. 
+    Create object of class Output_file.
+"""
+
+if __name__ == '__main__':
+    arguments = docopt(__doc__)
+    print(arguments)
+
+    output_file = Output_file(arguments)
+
+    if output_file.read_input_files() is False:
+        print(output_file.error_message)
+    else:
+        output_file.write_output_file()
+
+    print("--- %s seconds ---" % (time.time() - start_time))
